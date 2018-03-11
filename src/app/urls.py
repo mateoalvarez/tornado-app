d57d24a490c7c1e84c09ef6d96a985f9b8859c2d
mateyoalvarez <mateyoalvarez@gmail.com>
2018-03-11 18:08:58 +0100

    Added basic handlers, routes and templates for datasets, ml models and trained ml models

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,6 +4,9 @@ from tornado.web import url
 
 from models.home.handlers import HomeHandler
 from models.users.handlers import RegisterHandler, LogoutHandler, LoginHandler
+from models.datasets.handlers import DatasetsHandler
+from models.ml_models.handlers import MLModelsHandler
+from models.trained_ml_models.handlers import TrainedMLModelsHandler
 
 URL_PATTERNS = [
     # Home
@@ -14,6 +17,12 @@ URL_PATTERNS = [
     url(r"/auth/login", LoginHandler, name="login"),
     url(r"/auth/logout", LogoutHandler, name="logout"),
 
-    #
+    # datasets
+    url(r"/datasets", DatasetsHandler, name="datasets"),
 
+    # ml_models
+    url(r"/ml_models", MLModelsHandler, name="ml_models"),
+
+    # trained_ml_models
+    url(r"/trained_ml_models", TrainedMLModelsHandler, name="trained_ml_models"),
 ]
