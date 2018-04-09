93a53eab34e3ad7ef17da465f5da52acd707bc1e
mateyoalvarez <mateyoalvarez@gmail.com>
2018-04-10 01:01:24 +0200

    WIP refactor aws deploy

--- src/app/models/ml_models/aws_deploy_handler.py
+++ src/app/models/ml_models/aws_deploy_handler.py
@@ -9,7 +9,7 @@ class MLModelsAWSDeployHandler(BaseHandler):
     """Handler to deploy jobs on AWS"""
 
     @staticmethod
-    def _create_job_from_template(elements_to_replace):
+    def _create_job_json_from_template(elements_to_replace):
         """Create configuration from template and params"""
 
         json_template_file = requests.get(\
@@ -37,35 +37,18 @@ class MLModelsAWSDeployHandler(BaseHandler):
 
         return prereq_file
 
-    def _create_spark_job_file(self, pipeline):
+    def _create_application_job_file(self, application):
         """Generate spark job code"""
 
-        pipeline_stages = [pipeline.pipeline_prep_stages, pipeline.pipeline_models]
-        spark_job_file = ""
+        return
 
-        for pipeline_stage in pipeline_stages:
+    def _upload_emr_files(self, spark_job_file, prereq_file, application_id):
+        """UPLOAD files and return url"""
 
-            spark_job_file += self._get_spark_code_block(pipeline_stage).code_content + "\n"
-
-        #  TODO create file from options MODULABLE
-        # spark_job_file =\
-        #     '''
-        #         import pyspark
-        #         sc = pyspark.SparkContext()
-        #         data = [1, 2, 3, 4, 5]
-        #         distData = sc.parallelize(data)
-        #         sc.saveAsTextFile("s3://tornado-app-emr/mateyo@msn.com/1/log/file")
-        #     '''
-        return spark_job_file
-
-        def _upload_emr_files(self, spark_job_file, prereq_file, job_id):
-            """UPLOAD files and return url"""
-            pass
-
-        prereq_file_url = "{user}/{job_id}/prerequisites_{job_id}.sh".\
-            format(user=self.current_user["email"], job_id=job_id)
-        spark_job_file_url = "{user}/{job_id}/spark_job_{job_id}".\
-            format(user=self.current_user["email"], job_id=job_id)
+        prereq_file_url = "{user}/{application_id}/prerequisites_{application_id}.sh".\
+            format(user=self.current_user["email"], application_id=application_id)
+        spark_job_file_url = "{user}/{application_id}/spark_job_{application_id}".\
+            format(user=self.current_user["email"], application_id=application_id)
 
         s3_client, _ = self.start_s3_connection()
 
@@ -90,17 +73,17 @@ class MLModelsAWSDeployHandler(BaseHandler):
 
         return spark_job_file_url, prereq_file_url
 
-    def _deploy_emr_pipeline_training(self, pipeline, spark_job_file_url, prereq_file_url):
-        """DEPLOT pipeline on cluster"""
+    def _deploy_emr_application_training(self, application, spark_job_file_url, prereq_file_url):
+        """DEPLOT application on cluster"""
 
         elements_to_replace = {
             "user": self.current_user["email"],
             "spark-job": spark_job_file_url,
-            "job_id": pipeline["id"],
+            "application_id": application["id"],
             "preconfig_script": prereq_file_url
         }
 
-        job_step_content = self._create_job_from_template(elements_to_replace=elements_to_replace)
+        job_step_content = self._create_job_json_from_template(elements_to_replace=elements_to_replace)
 
         emr_client = self.start_emr_connection()
         result = emr_client.run_job_flow(**job_step_content)
@@ -108,7 +91,7 @@ class MLModelsAWSDeployHandler(BaseHandler):
 
 
     def get(self):
-        """GET pipeline deployment information"""
+        """GET application deployment information"""
 
     def post(self):
         """CREATE deployment on AWS"""
@@ -122,10 +105,10 @@ class MLModelsAWSDeployHandler(BaseHandler):
         application = self.db_cur.fetchone()
         print(application)
 
-        # spark_job_file, prereq_file = self._create_emr_files(pipeline)
+        # spark_job_file, prereq_file = self._create_emr_files(application)
 
-        # spark_job_file_url, prereq_file_url = self._upload_emr_files(\
+        spark_job_file_url, prereq_file_url = self._upload_emr_files(spark_job_file, prereq_file, application_id)
 
-        # self._deploy_emr_pipeline_training(pipeline, spark_job_file_url, prereq_file_url)
+        # self._deploy_emr_application_training(application, spark_job_file_url, prereq_file_url)
 
         self.redirect(self.get_argument("next", "/ml_models"))
