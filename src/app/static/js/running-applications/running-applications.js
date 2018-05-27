13b4ad171a157ad6d8ba0867d87f2257c874f43a
mateyoalvarez <mateyoalvarez@gmail.com>
2018-05-27 17:00:25 +0200

    Download button call method

--- src/app/static/js/running-applications/running-applications.js
+++ src/app/static/js/running-applications/running-applications.js
@@ -1,17 +1,15 @@
 
 function downloadData(button){
-    console.log(" downloadData -> aqui veo " + $(button).val())
     $.ajax({
         url: "/running_applications/visualize",
         type: "POST",
         data: {
-            application_id:$('button').val()
+            application_id:button
         }
     })
 }
 
 function visualizeData(button){
-    console.log(" visualizeData -> aqui veo " + $(button).val())
     $.ajax({
         url: "/running_applications/visualize?app_name=" + $(button).val(),
         type: "GET"
